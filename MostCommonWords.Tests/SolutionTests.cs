5e2befba3b943b18900ada80254488e40a22099b
sagar <sagar.sahay@spektrix.com>
2020-04-08 16:47:51 +0100

    Common words different approach

--- MostCommonWords.Tests/SolutionTests.cs
+++ MostCommonWords.Tests/SolutionTests.cs
@@ -9,14 +9,17 @@ namespace MostCommonWords.Tests
         public void MostCommonWord_ReturnsMostCommonWord()
         {
             // Arrange
-            var paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.";
+            //var paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.";
 
-            var banned = new[] {"hit"};
+           // var banned = new[] {"hit"};
+           var paragraph = "Bob. hIt, baLl";
+
+           var banned = new string[] {"bob", "hit"};
             
             var sut = new Solution();
             
             // Act
-            var res = sut.MostCommonWord(paragraph, banned);
+             var res = sut.MostCommonWord(paragraph, banned);
             
             // Assert
             res.Should().Be("ball");
--- MostCommonWords/Solution.cs
+++ MostCommonWords/Solution.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Text;
+
 namespace MostCommonWords
 {
     using System.Collections.Generic;
@@ -8,27 +11,109 @@ namespace MostCommonWords
     {
         public string MostCommonWord(string paragraph, string[] banned)
         {
-            var para = paragraph.Split(' ');
+            var separators = new[] {',', '.', '/'};
+            var para = paragraph.Split(separators, StringSplitOptions.RemoveEmptyEntries);
             var countDict = new Dictionary<string, int>();
+            var bannedDict = new HashSet<string>(banned);
+            var count = 0;
+            var result = "";
             foreach (var s in para)
             {
-                foreach (var bannedString in banned)
+                var s1 = s.ToLowerInvariant().Trim();
+                if (bannedDict.Contains(s1))
                 {
-                    if (s.ToLower() != bannedString.ToLower())
-                    {
-                        if (countDict.ContainsKey(s.ToLower()))
-                        {
-                            countDict[s.ToLower()] += 1;
-                        }
-                        else
-                        {
-                            countDict.Add(s.ToLower(), 1);
-                        }
-                    }
+                    continue;
                 }
-                
+
+                if (string.IsNullOrEmpty(s1))
+                {
+                    continue;
+                }
+
+                if (countDict.ContainsKey(s1))
+                {
+                    countDict[s1] += 1;
+                }
+                else
+                {
+                    countDict.Add(s1, 1);
+                }
+                if (count < countDict[s1])
+                {
+                    count = countDict[s1];
+                    result = s1;
+                }
             }
-            return countDict.OrderByDescending(x => x.Value).First().Key;
+
+            return result;
+        }
+
+        // public string MostCommonWord(string paragraph, string[] banned)
+        // {
+        //     var bannedSet = new HashSet<string>(banned);
+        //     var answerFreqDict = new Dictionary<string, int>();
+        //     var answerFrequency = 0;
+        //     var result = "";
+        //     var sb = new StringBuilder();
+        //
+        //     foreach (var c in paragraph.ToCharArray())
+        //     {
+        //         if (Char.IsLetter(c))
+        //         {
+        //             sb.Append(c);
+        //         }
+        //         if (sb.ToString().Trim().Length > 0)
+        //         {
+        //             var word = sb.ToString().ToLower();
+        //             if (bannedSet.Contains(word) || string.IsNullOrEmpty(word))
+        //             {
+        //                 sb = new StringBuilder();
+        //                 continue;
+        //             }
+        //
+        //             if (answerFreqDict.ContainsKey(word))
+        //             {
+        //                 answerFreqDict[word] += 1;
+        //             }
+        //             else
+        //             {
+        //                 answerFreqDict.Add(word, 1);
+        //             }
+        //
+        //             if (answerFreqDict[word] > answerFrequency)
+        //             {
+        //                 answerFrequency = answerFreqDict[word];
+        //                 result = word;
+        //             }
+        //
+        //             sb = new StringBuilder();
+        //         }
+        //     }
+        //
+        //     return result;
+        // }
+    }
+
+    public static class StringHelper
+    {
+        public static string RemovePunctuation(this string s)
+        {
+            var charArray = s.ToCharArray();
+            var sb = new StringBuilder();
+
+            foreach (var c in charArray)
+            {
+                if (!Char.IsPunctuation(c))
+                {
+                    sb.Append(c);
+                }
+                else
+                {
+                    sb.Append(" ");
+                }
+            }
+
+            return sb.ToString();
         }
     }
 }
